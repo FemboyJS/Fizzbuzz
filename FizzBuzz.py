91c62b26bfead4fa85501e88b34bf2146535aaf0
Luke Borrett <alphatomcattm@gmail.com>
2018-09-10 19:43:12 +0100

    JavaScript and Python added

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+def Check():
+	if x%3 == 0 and x%5 == 0:
+		print("FizzBuzz")
+	elif x%3 == 0:
+		print("Fizz")
+	elif x%5 == 0:
+		print("Buzz")
+	else:
+		print(x)
+for x in range(1,101):
+	Check()
